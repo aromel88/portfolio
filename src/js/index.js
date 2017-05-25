76f4d33312956dde8e00a8363339853292749495
Aaron Romel <aromel8838@gmail.com>
2017-05-25 09:00:26 -0400

    Environment set up

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+
+import '../css/1200grid-12column.css'
+import '../css/styles.css';
+
+const init = () => {
+  console.log('yay!');
+};
+
+window.addEventListener('load', init);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require("webpack");
+var path = require('path');
+//require("url-loader?prefix=img/!./bg.jpg");
+
+module.exports = {
+	entry: "./src/js/index.js",
+	output: {
+		path: path.resolve('./'),
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: "babel-loader",
+				query: {
+					presets: ["env", "latest"]
+				}
+			},
+			{
+				test: /\.css$/,
+        exclude: /node_modules/,
+				loader: 'style-loader!css-loader!postcss-loader'
+			},
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        exclude: /node_modules/,
+        loader: 'url-loader?name=/img/[name].[ext]'
+      }
+		]
+	}
+}
