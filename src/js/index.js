dc6b2d5e300de21d6ce27a71946e56da7a1cab53
Aaron Romel <aromel8838@gmail.com>
2017-05-30 15:35:10 -0400

    Added tweenlite animation of window scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
 
 import '../css/styles.scss';
 
+let nav;
+
+const navSlide = () => {
+  TweenLite.to(window, 0.8, {scrollTo: {y: '#toolbelt' }});
+};
+
 const init = () => {
-  console.log('yay!');
+  nav = document.querySelector('.nav-hover');
+  nav.addEventListener('click', navSlide);
 };
 
 window.addEventListener('load', init);
